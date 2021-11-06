fc591f032c4025b89e1b076570522c9a178334b2
KorolevaEkaterina334740 <90193181+KorolevaEkaterina334740@users.noreply.github.com>
2021-11-06 15:22:41 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,67 +1,64 @@
 import typing as tp
 
 
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    ciphertext = ""
+def encrypt_vigenere(unciphered_text: str, keyword: str) -> str:
+
+    ciphered_text = ""
     a = 0
-    while len(plaintext) > len(keyword):
+    while len(unciphered_text) > len(keyword):
         keyword += keyword[a]
         a += 1
     for i in range(len(keyword)):
         if keyword[i].isupper():
             key = ord(keyword[i]) - 65
-        elif keyword[i].islower():
-            key = ord(keyword[i]) - 97
-        if plaintext[i].isalpha():
-            c = ord(plaintext[i])
-            if plaintext[i].isupper() and c >= 91 - key:
-                ciphertext += chr(c - 26 + key)
-            elif plaintext[i].islower() and c >= 123 - key:
-                ciphertext += chr(c - 26 + key)
-            else:
-                ciphertext += chr(c + key)
         else:
-            ciphertext += plaintext[i]
-    return ciphertext
+            if keyword[i].islower():
+                key = ord(keyword[i]) - 97
+        if unciphered_text[i].isalpha():
+            c = ord(unciphered_text[i])
+            if unciphered_text[i].isupper() and c >= 91 - key:
+                ciphered_text += chr(c - 26 + key)
+            else:
+                if unciphered_text[i].islower() and c >= 123 - key:
+                    ciphered_text += chr(c - 26 + key)
+                else:
+                    ciphered_text += chr(c + key)
+        else:
+            ciphered_text += unciphered_text[i]
+    return ciphered_text
+
+'''e = encrypt_vigenere("PYTHON", "A") #'PYTHON'
+e = encrypt_vigenere("python", "a") #'python'
+e = encrypt_vigenere("ATTACKATDAWN", "LEMON") #'LXFOPVEFRNHR'
+print(e)'''
 
 
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    plaintext = ""
+def decrypt_vigenere(ciphered_text: str, keyword: str) -> str:
+    unciphered_text = ""
     a = 0
-    while len(ciphertext) > len(keyword):
+    while len(ciphered_text) > len(keyword):
         keyword += keyword[a]
         a += 1
     for i in range(len(keyword)):
         if keyword[i].isupper():
             key = ord(keyword[i]) - 65
-        elif keyword[i].islower():
-            key = ord(keyword[i]) - 97
-        if ciphertext[i].isalpha():
-            c = ord(ciphertext[i])
-            if ciphertext[i].isupper() and c <= 64 + key:
-                plaintext += chr(c + 26 - key)
-            elif ciphertext[i].islower() and c <= 96 + key:
-                plaintext += chr(c + 26 - key)
-            else:
-                plaintext += chr(c - key)
         else:
-            plaintext += ciphertext[i]
-    return plaintext
+            if keyword[i].islower():
+                key = ord(keyword[i]) - 97
+        if ciphered_text[i].isalpha():
+            c = ord(ciphered_text[i])
+            if ciphered_text[i].isupper() and c <= 64 + key:
+                unciphered_text += chr(c + 26 - key)
+            else:
+                if ciphered_text[i].islower() and c <= 96 + key:
+                    unciphered_text += chr(c + 26 - key)
+                else:
+                    unciphered_text += chr(c - key)
+        else:
+            unciphered_text += ciphered_text[i]
+    return unciphered_text
+
+'''d = decrypt_vigenere("PYTHON", "A") #'PYTHON'
+d = decrypt_vigenere("python", "a") #'python'
+d = decrypt_vigenere("LXFOPVEFRNHR", "LEMON") #'ATTACKATDAWN'
+print(d)'''
