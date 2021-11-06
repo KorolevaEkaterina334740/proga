7663874b802990c465564104c6d18a4779aeb017
KorolevaEkaterina334740 <90193181+KorolevaEkaterina334740@users.noreply.github.com>
2021-11-06 15:18:09 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -3,39 +3,26 @@ import typing as tp
 
 
 def is_prime(n: int) -> bool:
-    """
-    Tests to see if a number is prime.
-    >>> is_prime(2)
-    True
-    >>> is_prime(11)
-    True
-    >>> is_prime(8)
-    False
-    """
     if n == 2:
         return True
-    elif n == 1:
-        return False
+    else:
+        if n == 1:
+            return False
     if n % 2 == 0:
         return False
-    d = 3
-    while d ** 2 <= n and n % d != 0:
-        d += 2
-    return d ** 2 > n
+    b = 3
+    while b ** 2 <= n and n % b != 0:
+        b += 2
+    return b ** 2 > n
     pass
 
-
-# print (is_prime(1))
+'''i = is_prime(2) #True
+i = is_prime(11) #True
+i = is_prime(8) #False
+print(i)'''
 
 
 def gcd(a: int, b: int) -> int:
-    """
-    Euclid's algorithm for determining the greatest common divisor.
-    >>> gcd(12, 15)
-    3
-    >>> gcd(3, 7)
-    1
-    """
     while a != 0 and b != 0:
         if a > b:
             a = a % b
@@ -44,74 +31,59 @@ def gcd(a: int, b: int) -> int:
     return a + b
     pass
 
+'''g = gcd(12, 15) #3
+g = gcd(3, 7) #1
+print(g)'''
+
 
 def multiplicative_inverse(e: int, phi: int) -> int:
-    """
-    Euclid's extended algorithm for finding the multiplicative
-    inverse of two numbers.
-    >>> multiplicative_inverse(7, 40)
-    23
-    """
-    x, xx, y, yy = 1, 0, 0, 1
+    x = 1
+    xx = 0
+    y = 0
+    yy = 1
     w = phi
     while phi:
         q = e // phi
-        e, phi = phi, e % phi
-        x, xx = xx, x - xx * q
-        y, yy = yy, y - yy * q
+        e = phi
+        phi = e % phi
+        x = xx
+        xx = x - xx * q
+        y = yy
+        yy = y - yy * q
     k = x % w
     return k
     pass
 
-
-# print(multiplicative_inverse(7, 40))
+'''m = multiplicative_inverse(7, 40) #23
+print(m)'''
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
-    elif p == q:
-        raise ValueError("p and q cannot be equal")
-
-    # n = pq
+    else:
+        if p == q:
+            raise ValueError("p and q cannot be equal")
     n = p * q
-
-    # phi = (p-1)(q-1)
     phi = (p - 1) * (q - 1)
-
-    # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
-
-    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
-
-    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
-
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
 
-def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
-    # Unpack the key into it's components
+def encrypt(pk: tp.Tuple[int, int], unciphered_text: str) -> tp.List[int]:
     key, n = pk
-    # Convert each letter in the plaintext to numbers based on
-    # the character using a^b mod m
-    cipher = [(ord(char) ** key) % n for char in plaintext]
-    # Return the array of bytes
+    cipher = [(ord(char) ** key) % n for char in unciphered_text]
     return cipher
 
 
-def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
-    # Unpack the key into its components
+def decrypt(pk: tp.Tuple[int, int], ciphered_text: tp.List[int]) -> str:
     key, n = pk
-    # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
-    # Return the array of bytes as a string
+    plain = [chr((char ** key) % n) for char in ciphered_text]
     return "".join(plain)
 
 
@@ -123,9 +95,9 @@ if __name__ == "__main__":
     public, private = generate_keypair(p, q)
     print("Your public key is ", public, " and your private key is ", private)
     message = input("Enter a message to encrypt with your private key: ")
-    encrypted_msg = encrypt(private, message)
+    encrypted_message = encrypt(private, message)
     print("Your encrypted message is: ")
-    print("".join(map(lambda x: str(x), encrypted_msg)))
+    print("".join(map(lambda x: str(x), encrypted_message)))
     print("Decrypting message with public key ", public, " . . .")
     print("Your message is:")
-    print(decrypt(public, encrypted_msg))
+    print(decrypt(public, encrypted_message))
