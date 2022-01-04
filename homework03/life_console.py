883b9be1d954e10d08dcd50a86dccfd0c5e8eaaa
KorolevaEkaterina334740 <90193181+KorolevaEkaterina334740@users.noreply.github.com>
2022-01-04 22:25:40 +0300

    Update life_console.py

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -1,22 +1,44 @@
 import curses
 
-from life import GameOfLife
-from ui import UI
-
+from life_game_files import GameOfLife
+from LG_ui import UI
 
 class Console(UI):
     def __init__(self, life: GameOfLife) -> None:
         super().__init__(life)
+        self.rows = len(self.life.curr_generation)
+        self.cols = len(self.life.curr_generation[0])
 
     def draw_borders(self, screen) -> None:
-        """ Отобразить рамку. """
-        pass
+        screen.border()
 
     def draw_grid(self, screen) -> None:
-        """ Отобразить состояние клеток. """
-        pass
+        for i in range(0, self.rows):
+            for j in range(0, self.cols):
+                symbol = "●" if self.life.curr_generation[i][j] else " "
+                screen.addch(i + 1, j + 1, symbol)
 
     def run(self) -> None:
-        screen = curses.initscr()
-        # PUT YOUR CODE HERE
-        curses.endwin()
+        curses.initscr()
+        screen = curses.newwin(self.rows + 3, self.cols + 3, 0, 0)
+        self.draw_borders(screen)
+
+        try:
+            while self.life.is_changing or not self.life.is_max_generations_exceeded:
+                self.draw_borders(screen)
+                self.draw_grid(screen)
+                screen.refresh()
+                self.life.step()
+                curses.napms(750)#to have chance to notice the difference
+        finally:
+            curses.endwin()
+
+
+def main():
+    game = GameOfLife(size=(20, 45))
+    app = Console(game)
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
